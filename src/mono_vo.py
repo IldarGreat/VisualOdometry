670dd182c9017dc152ea460d78905702fc83baca
Ildar Great <ildarthegreat@gmail.com>
2023-10-25 21:07:29 +0400

    Add new detectors

--- src/mono_vo.py
+++ src/mono_vo.py
@@ -4,7 +4,6 @@ import os.path
 import random
 from copy import copy
 
-import cv2
 from matplotlib import pyplot as plt
 from numpy import sort
 
@@ -22,8 +21,7 @@ class IndirectVisualOdometry:
         self.logging, self.see_tracking = None, None
         self.init_settings_parameters()
 
-        self.detector, self.descriptor, self.mather, self.opticalFlow, self.methodForComputingE = None, None, None, None, None
-        self.init_odometry_parameters()
+        self.detector, self.descriptor, self.mather, self.opticalFlow, self.methodForComputingE = init_odometry_params()
 
         self.cap, self.images, self.ground_truth_path = None, None, None
         self.init_data_parameters()
@@ -38,20 +36,29 @@ class IndirectVisualOdometry:
         if self.logging is True:
             logging.info(message)
 
+    def draw_matches(self, previous_image, current_image, matches):
+        if self.see_tracking:
+            matched_images = cv2.drawMatches(previous_image, self.previous_feature.features, current_image,
+                                             self.current_feature.features, matches[:50], None,
+                                             flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            cv2.imshow("Matched images", matched_images)
+            cv2.waitKey(1)
+
     def init_settings_parameters(self):
         logs, see_tracking = get_settings_information_from_config()
         if logs:
             logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
                                 format="%(asctime)s %(levelname)s %(message)s")
+            logging.getLogger().addHandler(logging.StreamHandler())
             self.logging = True
             self.log("Logging is set to True")
 
-        if see_tracking:
+        if see_tracking != 'None':
             self.see_tracking = True
             self.log("See tracking is set to True")
 
     def init_data_parameters(self):
-        camera_port, video_path, images_path, ground_truth_path = get_data_information_from_config()
+        camera_port, video_path, images_path, ground_truth_path, ground_truth_template = get_data_information_from_config()
         data_init = 0
         if camera_port != 'None':
             self.cap = cv2.VideoCapture(camera_port)
@@ -82,7 +89,7 @@ class IndirectVisualOdometry:
                 raise Exception("Unable to find images in: {}".format(images_path))
 
         if ground_truth_path != 'None':
-            if os.path.exists(images_path):
+            if os.path.exists(ground_truth_path):
                 print(0)  # TODO
                 self.ground_truth_path = ground_truth_path
             else:
@@ -90,55 +97,6 @@ class IndirectVisualOdometry:
 
         if data_init != 1:
             raise Exception("Only one data entry is required")
-
-    def init_odometry_parameters(self):
-        detector_name, descriptor_name, matcher_name, opticalFlow_name, methodForComputingE_name = get_odometry_information_from_config()
-        init_parameters = 0
-        norm_type_for_BFM = None
-        index_params_forFLANN, search_params_forFLANN = None, dict(checks=50)
-
-        if descriptor_name == 'ORB':
-            self.descriptor = cv2.ORB_create(3000)
-            norm_type_for_BFM = cv2.NORM_HAMMING
-            index_params_forFLANN = dict(algorithm=6, table_number=6, key_size=12, multi_probe_level=1)
-            init_parameters += 1
-        elif descriptor_name == 'SIFT':
-            self.descriptor = cv2.SIFT_create(3000)
-            norm_type_for_BFM = cv2.NORM_HAMMING
-            index_params_forFLANN = dict(algorithm=1, trees=5)
-            init_parameters += 1
-
-        if detector_name == 'ORB':
-            self.detector = cv2.ORB_create(3000)
-            init_parameters += 1
-            self.log("Using ORB detector")
-        elif detector_name == 'SIFT':
-            self.detector = cv2.SIFT_create(3000)
-            init_parameters += 1
-
-        if matcher_name == 'BFMatcher':
-            self.mather = cv2.BFMatcher(norm_type_for_BFM, crossCheck=True)
-            init_parameters += 1
-        elif matcher_name == 'FLANN':
-            self.mather = cv2.FlannBasedMatcher(index_params_forFLANN, search_params_forFLANN)
-            init_parameters += 1
-
-        if opticalFlow_name == 'None':
-            self.opticalFlow = opticalFlow_name
-            init_parameters += 1
-
-        if methodForComputingE_name == 'RANSAC':
-            self.methodForComputingE = cv2.RANSAC
-            init_parameters += 1
-        elif methodForComputingE_name == 'LMEDS':
-            self.methodForComputingE = cv2.LMEDS
-            init_parameters += 1
-
-        if init_parameters != 5:
-            raise Exception(
-                'One of odometry parameters not allowed {} {} {} {} {}'.format(detector_name, descriptor_name,
-                                                                               matcher_name, opticalFlow_name,
-                                                                               methodForComputingE_name))
 
     def get_frame(self, index):
         if self.images is not None:
@@ -197,23 +155,24 @@ class IndirectVisualOdometry:
             R, t = self.recover_pose(matched_features_from_previous_image, matched_features_from_current_image)
             self.position.update(R, t)
 
-            matched_images = cv2.drawMatches(previous_image, self.previous_feature.features, current_image,
-                                             self.current_feature.features, matches[:50], None,
-                                             flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-            cv2.imshow("Matched images2", matched_images)
-            cv2.waitKey(1)
-            print("x: {} \n y: {} \n z: {} \n index: {}".format(self.position.get_current_x(),
-                                                                self.position.get_current_y(),
-                                                                self.position.get_current_z(), index))
+            self.draw_matches(previous_image, current_image, matches)
+
+            self.log("\nx: {}\n y: {}\n z: {}\n index: {}".format(self.position.get_current_x(),
+                                                                  self.position.get_current_y(),
+                                                                  self.position.get_current_z(), index))
             index += 1
             self.previous_feature = copy(self.current_feature)
             previous_image = copy(current_image)
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.set_title(
+            "Detector: {},\ndescriptor: {},\nmatcher: {}".format("ORB", "ORB", "FLANN"))
+        plt.plot(odometry.position.x_arr, odometry.position.y_arr)
+        plt.show()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     camera = Camera(get_camera_from_config())
     odometry = IndirectVisualOdometry()
     odometry.start()
-    plt.scatter(odometry.position.x_arr, odometry.position.y_arr)
-    plt.show()
-    cv2.destroyAllWindows()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import yaml
-
+import cv2
 
 def get_camera_from_config():
     with open('../config.yaml', 'r') as file:
@@ -33,7 +33,8 @@ def get_data_information_from_config():
     video_path = config['data']['video']
     images_path = config['data']['images']
     ground_truth_path = config['data']['groundTruth']
-    return camera_port, video_path, images_path, ground_truth_path
+    ground_truth_template = config['data']['groundTruthTemplate']
+    return camera_port, video_path, images_path, ground_truth_path, ground_truth_template
 
 
 def get_settings_information_from_config():
@@ -42,3 +43,79 @@ def get_settings_information_from_config():
     logs = config['settings']['logs']
     see_tracking = config['settings']['seeTracking']
     return logs, see_tracking
+
+def init_odometry_params():
+    detector_name, descriptor_name, matcher_name, opticalFlow_name, methodForComputingE_name = get_odometry_information_from_config()
+    init_parameters = 0
+    norm_type_for_BFM = None
+    index_params_forFLANN, search_params_forFLANN = None, dict(checks=50)
+
+    if descriptor_name == 'ORB':
+        descriptor = cv2.ORB_create(3000)
+        norm_type_for_BFM = cv2.NORM_HAMMING
+        index_params_forFLANN = dict(algorithm=6, table_number=6, key_size=12, multi_probe_level=1)
+        init_parameters += 1
+    elif descriptor_name == 'SIFT':
+        descriptor = cv2.SIFT_create(3000)
+        norm_type_for_BFM = cv2.NORM_L2
+        index_params_forFLANN = dict(algorithm=1, trees=5)
+        init_parameters += 1
+    elif descriptor_name == 'AKAZE':
+        descriptor = cv2.AKAZE_create()
+        norm_type_for_BFM = cv2.NORM_HAMMING
+        init_parameters += 1
+    elif descriptor_name == 'BRISK':
+        descriptor = cv2.BRISK_create(3000)
+        norm_type_for_BFM = cv2.NORM_L2
+        init_parameters += 1
+    elif descriptor_name == 'SURF':
+        descriptor = cv2.xfeatures2d.SURF_create(3000)
+        norm_type_for_BFM = cv2.NORM_L2
+        init_parameters += 1
+
+    if detector_name == 'ORB':
+        detector = cv2.ORB_create(3000)
+        init_parameters += 1
+    elif detector_name == 'SIFT':
+        detector = cv2.SIFT_create(3000)
+        init_parameters += 1
+    elif detector_name == 'AKAZE':
+        detector = cv2.AKAZE_create()
+        init_parameters += 1
+    elif detector_name == 'BRISK':
+        detector = cv2.BRISK_create()
+        init_parameters += 1
+    elif detector_name == 'FAST':
+        detector = cv2.FastFeatureDetector_create(3000)
+        init_parameters += 1
+    elif detector_name == 'BLOB':
+        detector = cv2.SimpleBlobDetector()
+        init_parameters += 1
+    elif detector_name == 'SURF':
+        detector = cv2.xfeatures2d.SURF_create(3000)
+        init_parameters += 1
+
+    if matcher_name == 'BFMatcher':
+        mather = cv2.BFMatcher(norm_type_for_BFM, crossCheck=True)
+        init_parameters += 1
+    elif matcher_name == 'FLANN':
+        mather = cv2.FlannBasedMatcher(index_params_forFLANN, search_params_forFLANN)
+        init_parameters += 1
+
+    if opticalFlow_name == 'None':
+        opticalFlow = opticalFlow_name
+        init_parameters += 1
+
+    if methodForComputingE_name == 'RANSAC':
+        methodForComputingE = cv2.RANSAC
+        init_parameters += 1
+    elif methodForComputingE_name == 'LMEDS':
+        methodForComputingE = cv2.LMEDS
+        init_parameters += 1
+
+    if init_parameters != 5:
+        raise Exception(
+            'One of odometry parameters not allowed {} {} {} {} {}'.format(detector_name, descriptor_name,
+                                                                           matcher_name, opticalFlow_name,
+                                                                           methodForComputingE_name))
+    return descriptor, detector, mather, opticalFlow, methodForComputingE
